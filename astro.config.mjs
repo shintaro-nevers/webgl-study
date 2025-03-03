b8c2510f3518cf953e2a246bda17f55c2d327f62
shintaro tokuyama <tokuyama@nevers.jp>
2025-03-04 02:03:27 +0900

    ワークフロー設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://shintaro-nevers.github.io/webgl-study/',
+	base: '/webgl-study/',
+});
